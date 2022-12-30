ad42205a9c76b78f2583f87588702c1e399da904
muhammeddemirci <mhdemirci571@gmail.com>
2022-12-30 23:52:51 +0100

    14

---
+++ JAVASCRIPT/SMALL_ASSIGNMENTS/work14.js
@@ -0,0 +1,61 @@
+// ? =====================================
+// ?           FUNCTIONS
+// ? ======================================
+
+// CALISMA SORULARI 2
+
+// Soru-1  1000 ile 2000 arasındaki 13 e bölümünden 3 kalan sayıları konsola yazdırınız ?
+
+// let sayı;
+// let line="";
+// for (let index = 1000; index < 2000; index++)
+// {
+//    if (index%13===3) {
+//     sayı=index;
+//     line+=(sayı+"\t")
+
+//    }
+
+//     }
+//     console.log(line)
+
+
+
+
+
+// Soru-2  Kullanıcıdan alınan 5 sayının ortalamasını konsola yazdırınız ?
+// let sayı=[];
+// x=0;
+// let toplam=0;
+// do {
+
+//     sayı[x]=+prompt("sayı giriniz")
+
+//     x++
+//     } while (x<5);
+
+// for(i=0; i<sayı.length;i++)
+// {
+//     toplam+=sayı[i]
+// }
+// toplam=toplam/5
+//     console.log(toplam);
+
+
+
+
+// Soru-3 Kullanıcıdan alınan sayıları negatif girilene kadar toplayan ve konsola yazan kodu yazınız ?
+// let x=0;
+// let toplam=0;
+
+//  do
+//  {
+//     x=+prompt("sayı giriniz")
+//     if (x<0) {
+//          console.log(`girilen sayı (${x}); negatiftir.`)
+//     } else {
+//         toplam+=x
+//         console.log(toplam)
+//     }
+// }
+// while (0<=x)
