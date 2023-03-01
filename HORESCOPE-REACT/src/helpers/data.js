f4dd32ccc70b318cddf64f58a8867273d5a2b272
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-01 21:36:23 +0100

    fix

--- HORESCOPE-REACT/src/helpers/data.js
+++ HORESCOPE-REACT/src/helpers/data.js
@@ -37,7 +37,7 @@ const data = [
   {
     id: 5,
     title: "Leo",
-    date:"(July 23 - August 22)",
+    date:"(July 23 - Aug 22)",
     desc:
       "Roll out the red carpet because Leo has arrived. Passionate, loyal, and infamously dramatic, Leo is represented by the lion and these spirited fire signs are the kings and queens of the celestial jungle. They're delighted to embrace their royal status: Vivacious, theatrical, and fiery, Leos love to bask in the spotlight and celebrate… well, themselves.",
     image: "https://drscdn.500px.org/photo/68303317/q%3D80_m%3D2000/v2?sig=3a2f9aec5a2276090f6e3245b30a7d9e8d65008beb346d38e2ec77386483056d",
@@ -45,7 +45,7 @@ const data = [
   {
     id: 6,
     title: "Virgo",
-    date:"(August 23 - September 22)",
+    date:"(Aug 23 - Sep 22)",
     desc:
       "You know the expression, 'if you want something done, give it to a busy person?' Well, that definitely is the Virgo anthem. Virgos are logical, practical, and systematic in their approach to life. Virgo is an earth sign historically represented by the goddess of wheat and agriculture, an association that speaks to Virgo's deep-rooted presence in the material world. This earth sign is a perfectionist at heart and isn’t afraid to improve skills through diligent and consistent practice.",
     image:
@@ -54,7 +54,7 @@ const data = [
   {
     id: 7,
     title: "Libra",
-    date:"(September 23 - October 22)",
+    date:"(Sep 23 - Oct 22)",
     desc:
       "Balance, harmony, and justice define Libra energy. As a cardinal air sign, Libra is represented by the scales (interestingly, the only inanimate object of the zodiac), an association that reflects Libra's fixation on establishing equilibrium. Libra is obsessed with symmetry and strives to create equilibrium in all areas of life — especially when it comes to matters of the heart.",
     image:
@@ -63,7 +63,7 @@ const data = [
   {
     id: 8,
     title: "Scorpio",
-    date:"(October 23 - November 21)",
+    date:"(Oct 23 - Nov 21)",
     desc:
       "Elusive and mysterious, Scorpio is one of the most misunderstood signs of the zodiac. Scorpio is a water sign that uses emotional energy as fuel, cultivating powerful wisdom through both the physical and unseen realms. In fact, Scorpio derives its extraordinary courage from its psychic abilities, which is what makes this sign one of the most complicated, dynamic signs of the zodiac",
     image: "https://drscdn.500px.org/photo/1058508757/q%3D80_m%3D2000_k%3D1/v2?sig=e7d24d769ebb5f03ae9ccd24ce3493a1e014fffc9515f5430284ca5d5f425435",
@@ -71,7 +71,7 @@ const data = [
   {
     id: 9,
     title: "Sagittarius",
-    date:"(November 22 - December 21)",
+    date:"(Nov 22 - Dec 21)",
     desc:
       "Oh, the places Sagittarius goes! But… actually. This fire sign knows no bounds. Represented by the archer, Sagittarians are always on a quest for knowledge. The last fire sign of the zodiac, Sagittarius launches its many pursuits like blazing arrows, chasing after geographical,.",
     image:
@@ -80,7 +80,7 @@ const data = [
   {
     id: 10,
     title: "Capricorn",
-    date:"(December 22 - January 19)",
+    date:"(Dec 22 - Jan 19)",
     desc:
       "What is the most valuable resource? For Capricorn, the answer is clear: Time. Capricorn is climbing the mountain straight to the top and knows that patience, perseverance, and dedication is the only way to scale. The last earth sign of the zodiac, Capricorn, is represented by the sea-goat, a mythological creature with the body of a goat and the tail of a fish. Accordingly, Capricorns are skilled at navigating both the material and emotional realms.",
     image:
@@ -89,7 +89,7 @@ const data = [
   {
     id: 11,
     title: "Aquarius",
-    date:"(January 20 - February 18)",
+    date:"(Jan 20 - Feb 18)",
     desc:
       "Despite the 'aqua' in its name, Aquarius is actually the last air sign of the zodiac. Innovative, progressive, and shamelessly revolutionary, Aquarius is represented by the water bearer, the mystical healer who bestows water, or life, upon the land. Accordingly, Aquarius is the most humanitarian astrological sign. At the end of the day, Aquarius is dedicated to making the world a better place.",
     image: "https://drscdn.500px.org/photo/1049427126/q%3D80_m%3D2000/v2?sig=98f779475524b162f09c3859e9a7f60b676c244342ad0888c0cc1270482c941a",
@@ -97,7 +97,7 @@ const data = [
   {
     id: 12,
     title: "Pisces",
-    date:"(February 19 - March 20)",
+    date:"(Feb 19 - March 20)",
     desc:
       "If you looked up the word 'psychic' in the dictionary, there would definitely be a picture of Pisces next to it. Pisces is the most intuitive, sensitive, and empathetic sign of the entire zodiac — and that’s because it’s the last of the last. As the final sign, Pisces has absorbed every lesson — the joys and the pain, the hopes and the fears — learned by all of the other signs. It's symbolized by two fish swimming in opposite directions, representing the constant division of Pisces' attention between fantasy and reality.",
     image:
